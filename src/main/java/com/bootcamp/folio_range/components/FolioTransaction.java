b134854548da1989bdb723778065a743ec073a6c
saurabh-code <saurabh.kumar.code@gmail.com>
2018-08-10 10:26:04 +0530

    folio transaction tuple

---
+++ src/main/java/com/bootcamp/folio_range/components/FolioTransaction.java
@@ -0,0 +1,38 @@
+package com.bootcamp.folio_range.components;
+
+public class FolioTransaction {
+
+	private int start;
+	private int end;
+	private String statusCode;
+	private int transactionCode;
+	
+	public FolioTransaction(int start, int end, String statusCode, int transactionCode) {
+		this.start = start;
+		this.end = end;
+		this.statusCode = statusCode;
+		this.transactionCode = transactionCode;
+	}
+
+	public int getStart() {
+		return start;
+	}
+
+	public int getEnd() {
+		return end;
+	}
+
+	public String getStatusCode() {
+		return statusCode;
+	}
+
+	public int getTransactionCode() {
+		return transactionCode;
+	}
+
+	@Override
+	public String toString() {
+		return "FolioTransaction [start=" + start + ", end=" + end + ", statusCode=" + statusCode + ", transactionCode="
+				+ transactionCode + "]";
+	}
+}
