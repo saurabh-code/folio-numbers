c7c7ac16171f6b5e286c958b1b479eda8a71b1d1
Narmathaa L <narmathaa18@gmail.com>
2018-08-10 11:58:03 +0530

    integration of helper functions

--- src/main/java/com/bootcamp/folio_range/FolioHelperFunction.java
+++ src/main/java/com/bootcamp/folio_range/FolioHelperFunction.java
@@ -7,15 +7,40 @@ import java.util.List;
 
 public class FolioHelperFunction {
 
+    public static void interChange(FolioTransaction first, FolioTransaction second)
+    {
+        FolioTransaction temp = first;
+        first = second;
+        second = temp;
+    }
+    public static boolean splitCondition(FolioTransaction first, FolioTransaction second){
+        if((first.getStatusCode()!= second.getStatusCode() || first.getTransactionCode() != second.getTransactionCode()) && (second.getStart() > first.getStart() && second.getStart() < first.getEnd()))
+            return true;
+        else
+            return false;
+    }
     public static boolean checkForSplit(FolioTransaction first, FolioTransaction second){
+        if(first.getStart() > second.getStart()) {
+            interChange(first, second);
+        }
+        if(splitCondition(first,second)){
+                return true;
+        }
         return false;
     }
 
     public static boolean checkForCombine(FolioTransaction first, FolioTransaction second){
+        if(first.getStatusCode() == second.getStatusCode() && first.getTransactionCode()== second.getTransactionCode()) {
+            if(first.getStart() > second.getStart()) {
+                interChange(first, second);
+            }
+            return (first.getEnd() == (second.getStart())-1);
+        }
         return false;
     }
     public static List<FolioTransaction> split(FolioTransaction first, FolioTransaction second){
         List<FolioTransaction> list = new  ArrayList<FolioTransaction>();
+
         return list;
     }
     public static List<FolioTransaction> combine(FolioTransaction first, FolioTransaction second){
