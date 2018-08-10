85fc62f4c16cfab979467749a5174158c984a976
Narmathaa L <narmathaa18@gmail.com>
2018-08-10 11:01:46 +0530

    helper function definitions

---
+++ src/main/java/com/bootcamp/folio_range/FolioHelperFunction.java
@@ -0,0 +1,24 @@
+package com.bootcamp.folio_range;
+
+import com.bootcamp.folio_range.components.FolioTransaction;
+
+import java.util.ArrayList;
+
+public class FolioHelperFunction {
+
+    public static boolean checkForSplit(FolioTransaction first, FolioTransaction second){
+        return false;
+    }
+
+    public static boolean checkForCombine(FolioTransaction first, FolioTransaction second){
+        return false;
+    }
+    public static ArrayList<FolioTransaction> split(FolioTransaction first, FolioTransaction second){
+        ArrayList<FolioTransaction> list = new  ArrayList<FolioTransaction>();
+        return list;
+    }
+    public static ArrayList<FolioTransaction> combine(FolioTransaction first, FolioTransaction second){
+        ArrayList<FolioTransaction> list = new  ArrayList<FolioTransaction>();
+        return list;
+    }
+}
